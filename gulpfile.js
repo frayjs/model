2cc6d4e3efc8dc14032003c32a6b6c2403f398c0
Pau Fracés <pfraces@gmail.com>
2016-05-19 03:33:14 +0200

    cleaned build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var del = require('del');
 
-gulp.task('clean', function () {
-  return del(['tmp', 'dist']);
-});
-
-gulp.task('lint', ['clean'], function () {
+gulp.task('lint', function () {
   var sources = [
     'src/**/*.js',
     'test/**/*.js'
@@ -18,15 +13,4 @@ gulp.task('lint', ['clean'], function () {
     .pipe(eslint.failOnError());
 });
 
-gulp.task('scripts', ['lint'], function () {
-  return gulp.src('src/**/*.js')
-    .pipe(gulp.dest('tmp/build'));
-});
-
-gulp.task('build', ['scripts']);
-gulp.task('test', ['build']);
-
-gulp.task('dist', ['test'], function () {
-  return gulp.src('tmp/build/**')
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('test', ['lint']);
